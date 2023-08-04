5e1ce7a72827bb22099ee943b278bd0d902e6c33
Daniel P Florian <dpflorian@gmail.com>
2023-08-03 21:15:07 -0600

    Add definitions for print_functions_for_lab_checks.py

--- classify-pet-images/print_functions_for_lab_checks.py
+++ classify-pet-images/print_functions_for_lab_checks.py
@@ -1,113 +1,310 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-                                                                  
-# PROGRAMMER: Daniel Florian
-# DATE CREATED: 06/22/2023 
-# PURPOSE: Create a function print_results that prints the results statistics
-#          from the results statistics dictionary (results_stats_dic). It 
-#          should also allow the user to be able to print out cases of misclassified
-#          dogs and cases of misclassified breeds of dog using the Results 
-#          dictionary (results_dic).  
-#         This function inputs:
-#            -The results dictionary as results_dic within print_results 
-#             function and results for the function call within main.
-#            -The results statistics dictionary as results_stats_dic within 
-#             print_results function and results_stats for the function call within main.
-#            -The CNN model architecture as model wihtin print_results function
-#             and in_arg.arch for the function call within main. 
-#            -Prints Incorrectly Classified Dogs as print_incorrect_dogs within
-#             print_results function and set as either boolean value True or 
-#             False in the function call within main (defaults to False)
-#            -Prints Incorrectly Classified Breeds as print_incorrect_breed within
-#             print_results function and set as either boolean value True or 
-#             False in the function call within main (defaults to False)
-#         This function does not output anything other than printing a summary
-#         of the final results.
+# -*- coding: utf-8 -*-
+# */AIPND/intropylab-classifying-images/print_functions_for_lab_checks.py
+#                                                                             
+# PROGRAMMER: Jennifer S.                                                    
+# DATE CREATED: 05/14/2018                                  
+# REVISED DATE:             <=(Date Revised - if any)                         
+# PURPOSE:  This set of functions can be used to check your code after programming 
+#           each function. The top section of each part of the lab contains
+#           the section labeled 'Checking your code'. When directed within this
+#           section of the lab one can use these functions to more easily check 
+#           your code. See the docstrings below each function for details on how
+#           to use the function within your code.
+#
 ##
 
-def print_results(results_dic, results_stats_dic, model, 
-                  print_incorrect_dogs = False, print_incorrect_breed = False):
+# Functions below defined to help with "Checking your code", specifically
+# running these functions with the appropriate input arguments within the
+# main() funtion will print out what's needed for "Checking your code"
+#
+def check_command_line_arguments(in_arg):
     """
-    Prints summary results on the classification and then prints incorrectly 
-    classified dogs and incorrectly classified dog breeds if user indicates 
-    they want those printouts (use non-default values)
+    For Lab: Classifying Images - 7. Command Line Arguments
+    Prints each of the command line arguments passed in as parameter in_arg, 
+    assumes you defined all three command line arguments as outlined in 
+    '7. Command Line Arguments'
+    Parameters:
+     in_arg -data structure that stores the command line arguments object
+    Returns:
+     Nothing - just prints to console  
+    """
+    if in_arg is None:
+        print("* Doesn't Check the Command Line Arguments because 'get_input_args' hasn't been defined.")
+    else:
+        # prints command line agrs
+        print("Command Line Arguments:\n     dir =", in_arg.dir, 
+              "\n    arch =", in_arg.arch, "\n dogfile =", in_arg.dogfile)
+
+def check_creating_pet_image_labels(results_dic):
+    """    For Lab: Classifying Images - 9/10. Creating Pet Image Labels
+    Prints first 10 key-value pairs and makes sure there are 40 key-value 
+    pairs in your results_dic dictionary. Assumes you defined the results_dic
+    dictionary as was outlined in 
+    '9/10. Creating Pet Image Labels'
+    Parameters:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+    Returns:
+     Nothing - just prints to console  
+    """
+    if results_dic is None:
+        print("* Doesn't Check the Results Dictionary because 'get_pet_labels' hasn't been defined.")
+    else:
+        # Code to print 10 key-value pairs (or fewer if less than 10 images)
+        # & makes sure there are 40 pairs, one for each file in pet_images/
+        stop_point = len(results_dic)
+        if stop_point > 10:
+            stop_point = 10
+        print("\nPet Image Label Dictionary has", len(results_dic),
+              "key-value pairs.\nBelow are", stop_point, "of them:")
+    
+        # counter - to count how many labels have been printed
+        n = 0
+    
+        # for loop to iterate through the dictionary
+        for key in results_dic:
+ 
+            # prints only first 10 labels
+            if n < stop_point:
+                print("{:2d} key: {:>30}  label: {:>26}".format(n+1, key,
+                      results_dic[key][0]) )
+
+                # Increments counter
+                n += 1
+            
+            # If past first 10 (or fewer) labels the breaks out of loop
+            else:
+                break
+
+
+def check_classifying_images(results_dic):
+    """    For Lab: Classifying Images - 11/12. Classifying Images
+    Prints Pet Image Label and Classifier Label for ALL Matches followed by ALL 
+    NOT matches. Next prints out the total number of images followed by how 
+    many were matches and how many were not-matches to check all 40 images are
+    processed. Assumes you defined the results_dic dictionary as was 
+    outlined in '11/12. Classifying Images'
     Parameters:
       results_dic - Dictionary with key as image filename and value as a List 
              (index)idx 0 = pet image label (string)
                     idx 1 = classifier label (string)
-                    idx 2 = 1/0 (int)  where 1 = match between pet image and 
-                            classifer labels and 0 = no match between labels
+                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
+                    classifer labels and 0 = no match between labels
+    Returns:
+     Nothing - just prints to console  
+
+    """
+    if results_dic is None:
+        print("* Doesn't Check the Results Dictionary because 'classify_images' hasn't been defined.")
+    elif len(results_dic[next(iter(results_dic))]) < 2:
+        print("* Doesn't Check the Results Dictionary because 'classify_images' hasn't been defined.")
+    else:
+        # Code for checking classify_images -
+        # Checks matches and not matches are classified correctly
+        # Checks that all 40 images are classified as a Match or Not-a Match
+    
+        # Sets counters for matches & NOT-matches
+        n_match = 0
+        n_notmatch = 0
+    
+        # Prints all Matches first
+        print("\n     MATCH:")
+        for key in results_dic:
+
+            # Prints only if a Match Index 2 == 1
+            if results_dic[key][2] == 1:
+
+                # Increments Match counter
+                n_match += 1
+                print("\nFilename: {} \nReal:       {}   \nClassifier: {}".format(key,
+                      results_dic[key][0], results_dic[key][1]))
+
+        # Prints all NOT-Matches next
+        print("\n NOT A MATCH:")
+        for key in results_dic:
+        
+            # Prints only if NOT-a-Match Index 2 == 0 
+            if results_dic[key][2] == 0:
+ 
+                # Increments Not-a-Match counter
+                n_notmatch += 1
+                print("\nFilename: {} \nReal:       {}   \nClassifier: {}".format(key,
+                      results_dic[key][0], results_dic[key][1]))
+
+        # Prints Total Number of Images - expects 40 from pet_images folder
+        print("\n# Total Images",n_match + n_notmatch, "# Matches:",n_match ,
+              "# NOT Matches:",n_notmatch)
+
+ 
+def check_classifying_labels_as_dogs(results_dic):
+    """    For Lab: Classifying Images - 13. Classifying Labels as Dogs
+    Prints Pet Image Label, Classifier Label, whether Pet Label is-a-dog(1=Yes,
+    0=No), and whether Classifier Label is-a-dog(1=Yes, 0=No) for ALL Matches 
+    followed by ALL NOT matches. Next prints out the total number of images 
+    followed by how many were matches and how many were not-matches to check 
+    all 40 images are processed. Assumes you defined the results_dic dictionary
+    as was outlined in '13. Classifying Labels as Dogs'
+    Parameters:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+                    idx 1 = classifier label (string)
+                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
+                    classifer labels and 0 = no match between labels
                     idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
                             0 = pet Image 'is-NOT-a' dog. 
                     idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
                             'as-a' dog and 0 = Classifier classifies image  
                             'as-NOT-a' dog.
-      results_stats_dic - Dictionary that contains the results statistics (either
-                   a  percentage or a count) where the key is the statistic's 
+    Returns:
+     Nothing - just prints to console  
+
+    """
+    if results_dic is None:
+        print("* Doesn't Check the Results Dictionary because 'adjust_results4_isadog' hasn't been defined.")
+    elif len(results_dic[next(iter(results_dic))]) < 4 :
+        print("* Doesn't Check the Results Dictionary because 'adjust_results4_isadog' hasn't been defined.")
+
+    else:
+        # Code for checking adjust_results4_isadog -
+        # Checks matches and not matches are classified correctly as "dogs" and
+        # "not-dogs" Checks that all 40 images are classified as a Match or Not-a 
+        # Match
+    
+        # Sets counters for matches & NOT-matches
+        n_match = 0
+        n_notmatch = 0
+    
+        # Prints all Matches first
+        print("\n     MATCH:")
+        for key in results_dic:
+
+            # Prints only if a Match Index 2 == 1
+            if results_dic[key][2] == 1:
+
+                # Increments Match counter
+                n_match += 1
+                print("\nFilename:{} \nReal:       {}   \nClassifier: {}  \nPetLabelDog: {:3d}  \nClassLabelDog: {:1d}".format(key,
+                      results_dic[key][0], results_dic[key][1], results_dic[key][3], 
+                      results_dic[key][4]))
+
+        # Prints all NOT-Matches next
+        print("\n NOT A MATCH:")
+        for key in results_dic:
+        
+            # Prints only if NOT-a-Match Index 2 == 0 
+            if results_dic[key][2] == 0:
+ 
+                # Increments Not-a-Match counter
+                n_notmatch += 1
+                print("\nFilename:{} \nReal:       {}   \nClassifier: {}  \nPetLabelDog: {:3d}  \nClassLabelDog: {:1d}".format(key,
+                      results_dic[key][0], results_dic[key][1], results_dic[key][3], 
+                      results_dic[key][4]))
+
+        # Prints Total Number of Images - expects 40 from pet_images folder
+        print("\n# Total Images",n_match + n_notmatch, "# Matches:",n_match ,
+              "# NOT Matches:",n_notmatch)
+
+
+
+def check_calculating_results(results_dic, results_stats_dic):
+    """    For Lab: Classifying Images - 14. Calculating Results
+    Prints First statistics from the results stats dictionary (that was created
+    by the calculates_results_stats() function), then prints the same statistics
+    that were calculated in this function using the results dictionary.
+    Assumes you defined the results_stats dictionary and the statistics 
+    as was outlined in '14. Calculating Results '
+    Parameters:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+                    idx 1 = classifier label (string)
+                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
+                    classifer labels and 0 = no match between labels
+                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
+                            0 = pet Image 'is-NOT-a' dog. 
+                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
+                            'as-a' dog and 0 = Classifier classifies image  
+                            'as-NOT-a' dog.
+     results_stats_dic - Dictionary that contains the results statistics (either
+                     a percentage or a count) where the key is the statistic's 
                      name (starting with 'pct' for percentage or 'n' for count)
                      and the value is the statistic's value 
-      model - Indicates which CNN model architecture will be used by the 
-              classifier function to classify the pet images,
-              values must be either: resnet alexnet vgg (string)
-      print_incorrect_dogs - True prints incorrectly classified dog images and 
-                             False doesn't print anything(default) (bool)  
-      print_incorrect_breed - True prints incorrectly classified dog breeds and 
-                              False doesn't print anything(default) (bool) 
     Returns:
-           None - simply printing results.
-    """    
+     Nothing - just prints to console  
 
-    # Prints summary statistics over the run
-    print("\n\n*** Results Summary for CNN Model Architecture",model.upper(), 
-          "***")
-    # Prints total number(N) of images
-    print("{:20}: {:3d}".format('N Images', results_stats_dic['n_images']))
-    # Print number(N) of dog images
-    print("{:20}: {:3d}".format('N Dog Images', results_stats_dic['n_dogs_img']))
-    # Prints number(N) of not dog images
-    print("{:20}: {:3d}".format('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
-
-    # Prints summary statistics (percentages) on Model Run
-    print(" ")
-    for key in results_stats_dic:
-
-        if key[0] == 'p':
-            print("{:20}: {:.2f}%".format(key, results_stats_dic[key]))
+    """
+    if results_stats_dic is None:
+        print("* Doesn't Check the Results Dictionary because 'calculates_results_stats' hasn't been defined.")
+    else:
+        # Code for checking results_stats_dic -
+        # Checks calculations of counts & percentages BY using results_dic
+        # to re-calculate the values and then compare to the values
+        # in results_stats_dic
     
-    # IF print_incorrect_dogs == True AND there were images incorrectly 
-    # classified as dogs or vice versa - print out these cases
-    if (print_incorrect_dogs and 
-       ((results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs']) 
-         != results_stats_dic['n_images'])):
-        print("\nINCORRECT Dog/NOT Dog Assignment(s):")
-
-        # process through results dict, printing incorrectly classified dogs
+        # Initialize counters to zero and number of images total
+        n_images = len(results_dic)
+        n_pet_dog = 0
+        n_class_cdog = 0
+        n_class_cnotd = 0
+        n_match_breed = 0
+    
+        # Interates through results_dic dictionary to recompute the statistics
+        # outside of the calculates_results_stats() function
         for key in results_dic:
 
-            # Pet Image Label is a Dog - Classified as NOT-A-DOG -OR- 
-            # Pet Image Label is NOT-a-Dog - Classified as a-DOG
-            if (results_dic[key][3] == 1 and results_dic[key][4] == 0) or \
-               (results_dic[key][3] == 0 and results_dic[key][4] == 1):
-                print("\nPet Image Label: {}\nClassifier Label: {}".format(results_dic[key][0],results_dic[key][1]))
+            # match (if dog then breed match)
+            if results_dic[key][2] == 1:
 
-    else:
-        if print_incorrect_dogs:
-            print("\nNo INCORRECT Dog/NOT Dog Assignment(s)")
+                # isa dog (pet label) & breed match
+                if results_dic[key][3] == 1:
+                    n_pet_dog += 1
 
-    # IF print_incorrect_breed == True AND there were dogs whose breeds 
-    # were incorrectly classified - print out these cases                    
-    if (print_incorrect_breed and 
-        (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed'])):
-        print("\nINCORRECT Dog Breed Assignment(s):")
+                    # isa dog (classifier label) & breed match
+                    if results_dic[key][4] == 1:
+                        n_class_cdog += 1
+                        n_match_breed += 1
 
-        # process through results dict, printing incorrectly classified breeds
-        for key in results_dic:
+                # NOT dog (pet_label)
+                else:
 
-            # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
-            if (sum(results_dic[key][3:]) == 2 and
-                results_dic[key][2] == 0):
-                print("\nReal:       {}\nClassifier: {}".format(results_dic[key][0],
-                                                          results_dic[key][1]))
+                    # NOT dog (classifier label)
+                    if results_dic[key][4] == 0:
+                        n_class_cnotd += 1
 
-    else:
-        if print_incorrect_breed:
-            print("\nNo INCORRECT Dog Breed Assignment(s)")
+            # NOT - match (not a breed match if a dog)
+            else:
+ 
+                # NOT - match
+                # isa dog (pet label) 
+                if results_dic[key][3] == 1:
+                    n_pet_dog += 1
+
+                    # isa dog (classifier label)
+                    if results_dic[key][4] == 1:
+                        n_class_cdog += 1
+
+                # NOT dog (pet_label)
+                else:
+
+                    # NOT dog (classifier label)
+                    if results_dic[key][4] == 0:
+                        n_class_cnotd += 1
+
+                    
+        # calculates statistics based upon counters from above
+        n_pet_notd = n_images - n_pet_dog
+        pct_corr_dog = ( n_class_cdog / n_pet_dog )*100
+        pct_corr_notdog = ( n_class_cnotd / n_pet_notd )*100
+        pct_corr_breed = ( n_match_breed / n_pet_dog )*100
+    
+        # prints calculated statistics
+        print("\n ** Statistics from calculates_results_stats() function:")
+        print("N Images: {:2d}  N Dog Images: {:2d}  N NotDog Images: {:2d} \nPct Corr dog: {:5.1f} Pct Corr NOTdog: {:5.1f}  Pct Corr Breed: {:5.1f}".format(
+              results_stats_dic['n_images'], results_stats_dic['n_dogs_img'],
+              results_stats_dic['n_notdogs_img'], results_stats_dic['pct_correct_dogs'],
+              results_stats_dic['pct_correct_notdogs'],
+              results_stats_dic['pct_correct_breed']))
+        print("\n ** Check Statistics - calculated from this function as a check:")
+        print("N Images: {:2d}  N Dog Images: {:2d}  N NotDog Images: {:2d} \nPct Corr dog: {:5.1f} Pct Corr NOTdog: {:5.1f}  Pct Corr Breed: {:5.1f}".format(
+              n_images, n_pet_dog, n_pet_notd, pct_corr_dog, pct_corr_notdog,
+              pct_corr_breed))
